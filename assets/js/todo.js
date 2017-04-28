026f3aa01e6c1c54e25ae9bfde47e26564390a7b
Zemog <jgomez630@aol.com>
2017-04-27 23:09:46 -0700

    first commit

---
+++ assets/js/todo.js
@@ -0,0 +1,35 @@
+//check off specific items off todo list
+$('li').click(function(){
+  $(this).toggleClass("completed");
+});
+
+//click on X to delete
+
+
+
+/*
+$("li").click(function(){
+  //console.log(this);
+  $(this).css('color','gray');
+  $(this).css('text-decoration','line-through');
+});
+*/
+
+/*
+$("li").click(function(){
+//console.log($(this).css('color'));
+  
+  if($(this).css('color') === 'rgb(128, 128, 128)'){
+     $(this).css({
+     color: "black",
+     textDecoration:"none",
+  });
+} else{
+  $(this).css({
+    color: "gray",
+    textDecoration:"line-through",
+  });
+}
+});
+*/
+
